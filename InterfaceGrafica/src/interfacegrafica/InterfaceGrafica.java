0772c92d90a78ebae1dfa31a70d12c858486e663
Paloma Nascimento <88505275+Paloma13744@users.noreply.github.com>
2023-10-20 08:51:26 -0300

    Add files via upload
    Poo I

---
+++ InterfaceGrafica/src/interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,15 @@
+package interfacegrafica;
+
+import javax.swing.JFrame;
+
+public class InterfaceGrafica {
+
+    public static void main(String[] args) {
+        //Primeiro projeto de interface gráfica;
+        JFrame janela = new JFrame();
+        janela.setTitle("Primeira tela");
+        janela.setSize(400, 200);
+        janela.setVisible(true);
+    }
+
+}
